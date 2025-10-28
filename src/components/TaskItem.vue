46ac0bdf9a9ea916a7a778577eeaa37843e4a7f5
sandrine-bd <sandrine.barrucand@le-campus-numerique.fr>
2025-10-28 11:38:45 +0100

    Création TaskItem.vue

---
+++ src/components/TaskItem.vue
@@ -0,0 +1,26 @@
+<template>
+    <li class="task-item">
+        <span :class="{ done: task.state === 'terminé' }">{{ task.description }}</span>
+        <div>
+            <button v-if="task.state !== 'terminé'" @click="$emit('complete', task.id)">Terminer</button>
+            <button @click="$emit('delete', task.id)">Supprimer</button>
+        </div>
+    </li>
+</template>
+
+<script setup>
+defineProps({ task: Object });
+</script>
+
+<style scoped>
+.task-item {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: 6px 0;
+}
+.done {
+    text-decoration: line-through;
+    color: gray;
+}
+</style>
